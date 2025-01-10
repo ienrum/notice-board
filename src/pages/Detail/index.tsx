7e407f64c872a698255ba6c60f6ec5267602669a
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2025-01-10 18:29:48 +0900

    feat: detail page 에 댓글 컴포넌트 적용

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,12 +1,16 @@
 import Header, { HeaderHome } from "@/components/ui/header";
+import CommentForm from "@/pages/Detail/components/CommentForm";
+import CommentList from "@/pages/Detail/components/CommentList";
 import DetailCard from "@/pages/Detail/components/DetailCard";
 
 const DetailPage = () => {
   return (
     <>
       <Header left={<HeaderHome />} />
-      <div className="flex justify-center min-w-96 min-h-96 p-8 gap-4">
+      <div className="flex flex-col justify-center min-w-96 min-h-96 p-8 gap-4">
         <DetailCard />
+        <CommentForm />
+        <CommentList />
       </div>
     </>
   );
