dd976dd38057c950b51bedf1e54bd6fab19c2817
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2025-01-10 18:28:52 +0900

    feat: home page 에 home link 제거

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import Header, { HeaderHome, HeaderProfile } from "@/components/ui/header";
+import Header, { HeaderProfile } from "@/components/ui/header";
 import ThreadList from "@/pages/Home/components/ThreadList";
 import ThreadPostFormModal from "@/pages/Home/components/ThreadPostModal";
 import { Suspense } from "react";
@@ -6,7 +6,7 @@ import { Suspense } from "react";
 const HomePage = () => {
   return (
     <>
-      <Header left={<HeaderHome />} right={<HeaderProfile />} />
+      <Header right={<HeaderProfile />} />
       <div className="p-8">
         <Suspense fallback={<div>Loading...</div>}>
           <ThreadList />
